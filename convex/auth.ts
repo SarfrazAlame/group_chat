b8e45010c40e9489c61f5afab88178cca2cef424
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-12-18 20:22:28 +0530

    creating logic for signing and signup pag

---
+++ convex/auth.ts
@@ -0,0 +1,10 @@
+import { convexAuth } from "@convex-dev/auth/server";
+import Google from "@auth/core/providers/google"
+import GitHub from "@auth/core/providers/github";
+import { Password } from "@convex-dev/auth/providers/Password";
+
+
+
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+    providers: [GitHub, Google,Password],
+});
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,13 @@
+'use client'
+
+
 import { Button } from "./ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { FcGoogle } from "react-icons/fc";
 import { SiGithub } from "react-icons/si";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 
 interface SignInProps {
@@ -12,11 +16,16 @@ interface SignInProps {
 }
 
 export default function SignIn({ auth, setAuth }: SignInProps) {
+    const { signIn } = useAuthActions()
     return (
         <div className="mt-20 border p-4 rounded-md  w-96">
             <div className="flex flex-col gap-8">
                 <p className="text-xl font-semibold text-slate-700"> Sign in to Continue Group Chat</p>
-                <form className="space-y-3">
+                <form className="space-y-3" onSubmit={(event) => {
+                    event.preventDefault()
+                    const formData = new FormData(event.currentTarget)
+                    void signIn('password', formData)
+                }}>
                     <Input placeholder="email" className="h-12" />
                     <Input placeholder="password" className="h-12" />
                     <p className="text-sm text-slate-600">don't have an account? <span className="text-blue-700 cursor-pointer hover:underline transition-all" onClick={() => setAuth('signup')}>signup</span></p>
@@ -24,8 +33,8 @@ export default function SignIn({ auth, setAuth }: SignInProps) {
                         <Button type="submit" variant={"outline"} >Signin</Button>
                     </div>
                     <div className="flex justify-evenly">
-                        <Button type="button" variant={'outline'} size={"lg"}><FcGoogle className="size-8"/></Button>
-                        <Button type="button" variant={'outline'} size={"lg"}><SiGithub className="size-8"/></Button>
+                        <Button type="button" variant={'outline'} size={"lg"}><FcGoogle className="size-8" /></Button>
+                        <Button type="button" variant={'outline'} size={"lg"}><SiGithub className="size-8" /></Button>
                     </div>
                 </form>
             </div>
